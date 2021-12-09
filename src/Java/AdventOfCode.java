ca0054ecd6330f8dd9486bc7a0d9c7b04cc83c50
Daniel Neubieser <corataca@outlook.de>
2021-12-10 00:08:54 +0100

    Day7 solved

--- src/Java/AdventOfCode.java
+++ src/Java/AdventOfCode.java
@@ -2,31 +2,35 @@ package Java;
 
 public class AdventOfCode {
     public static void main(String[] args) {
-        Day day1 = new Day1("inputDay1.txt");
-        day1.solveDayPartOne();
-        System.out.println(day1.solveDayPartOne());
-        day1.solveDayPartTwo();
-        System.out.println(day1.solveDayPartTwo());
+//        Day day1 = new Day1("inputDay1.txt");
+//        day1.solveDayPartOne();
+//        System.out.println(day1.solveDayPartOne());
+//        day1.solveDayPartTwo();
+//        System.out.println(day1.solveDayPartTwo());
+//
+//        Day day2 = new Day2("inputDay2.txt");
+//        System.out.println("Day2/1 " + day2.solveDayPartOne());
+//        System.out.println("Day2/2 " + day2.solveDayPartTwo());
+//
+//        Day day3 = new Day3("inputDay3.txt");
+//        System.out.println("Day3/1 " + day3.solveDayPartOne());
+//        System.out.println("Day3/2 " + day3.solveDayPartTwo());
+//
+//        Day day4 = new Day4("inputDay4.txt");
+//        System.out.println("Day4/1 " + day4.solveDayPartOne());
+//        System.out.println("Day4/2 " + day4.solveDayPartTwo());
+//
+//        Day day5 = new Day5("inputDay5.txt");
+//        System.out.println("Day5/1 " + day5.solveDayPartOne());
+//        System.out.println("Day5/2 " + day5.solveDayPartTwo());
+//
+//        Day day6 = new Day6("inputDay6.txt");
+//        System.out.println("Day6/1 " + day6.solveDayPartOne());
+//        System.out.println("Day6/2 " + day6.solveDayPartTwo());
 
-        Day day2 = new Day2("inputDay2.txt");
-        System.out.println("Day2/1 " + day2.solveDayPartOne());
-        System.out.println("Day2/2 " + day2.solveDayPartTwo());
-
-        Day day3 = new Day3("inputDay3.txt");
-        System.out.println("Day3/1 " + day3.solveDayPartOne());
-        System.out.println("Day3/2 " + day3.solveDayPartTwo());
-
-        Day day4 = new Day4("inputDay4.txt");
-        System.out.println("Day4/1 " + day4.solveDayPartOne());
-        System.out.println("Day4/2 " + day4.solveDayPartTwo());
-
-        Day day5 = new Day5("inputDay5.txt");
-        System.out.println("Day5/1 " + day5.solveDayPartOne());
-        System.out.println("Day5/2 " + day5.solveDayPartTwo());
-
-        Day day6 = new Day6("inputDay6.txt");
-        System.out.println("Day6/1 " + day6.solveDayPartOne());
-        System.out.println("Day6/2 " + day6.solveDayPartTwo());
+        Day day7 = new Day7("inputDay7.txt");
+        System.out.println("Day7/1 " + day7.solveDayPartOne());
+        System.out.println("Day7/2 " + day7.solveDayPartTwo());
 
     }
 }
